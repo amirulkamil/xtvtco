3c94b8e95f56a64c6c59fde9ac1a30d0251a270e
amirulkamil <amirul.kamil@gmail.com>
2018-03-19 18:50:31 +0800

    floating action button with animation

--- app/src/main/java/co/xtvt/xtvtco/ActivityDashboard.java
+++ app/src/main/java/co/xtvt/xtvtco/ActivityDashboard.java
@@ -1,11 +1,16 @@
 package co.xtvt.xtvtco;
 
+import android.os.Handler;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -16,29 +21,68 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import android.support.design.widget.TabLayout;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityDashboard extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private Boolean exit = false;
+    private AppBarLayout appBarLayout;
+    private NavigationView navigationView;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    private Toolbar toolbar;
+    FloatingActionButton fabPlus, fabActivity, fabPost, fabMedia;
+    Animation fabOpen, fabClose, fabRotateClockwise, fabRotateAntiClockwise;
+    Boolean isOpen = false;
+    View shadowView;
+    TextView textViewActivities, textViewPosts, textViewMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.home);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        shadowView = findViewById(R.id.shadowView);
+
+        fabPlus = findViewById(R.id.fab_plus);
+        fabActivity = findViewById(R.id.fab_activity);
+        fabPost = findViewById(R.id.fab_post);
+        fabMedia = findViewById(R.id.fab_media);
+
+        textViewActivities = findViewById(R.id.textViewActivities);
+        textViewPosts = findViewById(R.id.textViewPosts);
+        textViewMedia = findViewById(R.id.textViewMedia);
+
+        fabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fabRotateClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
+        fabRotateAntiClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anticlockwise);
+
+        fabPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fabVisibility();
+            }
+        });
+
+        /*fabPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -46,26 +90,105 @@ public class ActivityDashboard extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.viewpager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
-        bottomNavigationViewEx.enableItemShiftingMode(false);
-        bottomNavigationViewEx.enableShiftingMode(false);
-        bottomNavigationViewEx.setTextVisibility(false);
-        bottomNavigationViewEx.setIconSize(25,27);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        toolbar.setTitle(R.string.home);
+                        break;
+                    case 1:
+                        toolbar.setTitle(R.string.activities);
+                        break;
+                    case 2:
+                        toolbar.setTitle(R.string.people);
+                        break;
+                    case 3:
+                        toolbar.setTitle(R.string.posts);
+                        break;
+                    case 4:
+                        toolbar.setTitle(R.string.media);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    public void fabVisibility(){
+        if (isOpen){
+            fabActivity.startAnimation(fabClose);
+            textViewActivities.startAnimation(fabClose);
+            fabPost.startAnimation(fabClose);
+            textViewPosts.startAnimation(fabClose);
+            fabMedia.startAnimation(fabClose);
+            textViewMedia.startAnimation(fabClose);
+            fabPlus.startAnimation(fabRotateAntiClockwise);
+            fabActivity.setClickable(false);
+            fabPost.setClickable(false);
+            fabMedia.setClickable(false);
+            shadowView.setVisibility(View.GONE);
+            isOpen = false;
+        }else {
+            fabActivity.startAnimation(fabOpen);
+            textViewActivities.startAnimation(fabOpen);
+            fabPost.startAnimation(fabOpen);
+            textViewPosts.startAnimation(fabOpen);
+            fabMedia.startAnimation(fabOpen);
+            textViewMedia.startAnimation(fabOpen);
+            fabPlus.startAnimation(fabRotateClockwise);
+            fabActivity.setClickable(true);
+            fabPost.setClickable(true);
+            fabMedia.setClickable(true);
+            shadowView.setVisibility(View.VISIBLE);
+            isOpen = true;
+        }
+     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (shadowView.getVisibility() == View.VISIBLE) {
+            fabVisibility();
         } else {
-            super.onBackPressed();
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                if (exit) {
+                    finish(); // finish activity
+                } else {
+                    Toast.makeText(this, R.string.press_back_again_to_exit,
+                            Toast.LENGTH_SHORT).show();
+                    exit = true;
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            exit = false;
+                        }
+                    }, 3 * 1000);
+
+                }
+            }
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -95,13 +218,23 @@ public class ActivityDashboard extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_drawer_profile) {
-            // Handle the camera action
+            Intent intent = new Intent(ActivityDashboard.this, ActivityGeneral.class);
+            intent.putExtra("FragmentToLoad", "Profile");
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else if (id == R.id.nav_drawer_notifications) {
+            Intent intent = new Intent(ActivityDashboard.this, ActivityGeneral.class);
+            intent.putExtra("FragmentToLoad", "Notifications");
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         } else if (id == R.id.nav_drawer_activity) {
-
+            Intent intent = new Intent(ActivityDashboard.this, ActivityGeneral.class);
+            intent.putExtra("FragmentToLoad", "Activities");
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         } else if (id == R.id.nav_drawer_logout){
             logOutDialog();
         }
-
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -129,5 +262,41 @@ public class ActivityDashboard extends AppCompatActivity
         // Create the AlertDialog object and return it
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    FragmentMainHome fragmentMainHome = new FragmentMainHome();
+                    return fragmentMainHome;
+                case 1:
+                    FragmentMainActivities fragmentMainActivities = new FragmentMainActivities();
+                    return fragmentMainActivities;
+                case 2:
+                    FragmentMainPeople fragmentMainPeople = new FragmentMainPeople();
+                    return fragmentMainPeople;
+                case 3:
+                    FragmentMainPosts fragmentMainPosts = new FragmentMainPosts();
+                    return fragmentMainPosts;
+                case 4:
+                    FragmentMainMedia fragmentMainMedia = new FragmentMainMedia();
+                    return fragmentMainMedia;
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            // Show total pages.
+            return 5;
+        }
     }
 }
